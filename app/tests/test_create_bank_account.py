681c588db85ae548dd32cef83bfa5e78aab3040d
Julia <julia.naczke007@gmail.com>
2024-10-09 13:46:44 +0200

    feature2

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,10 +5,12 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
     imie = "Dariusz"
     nazwisko = "Januszewski"
+    pesel = '00281104942'
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto(self.imie, self.nazwisko)
+        pierwsze_konto = Konto(self.imie, self.nazwisko, self.pesel)
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, '00281104942', "Nie ma peselu" )
 
     #tutaj proszę dodawać nowe testy
